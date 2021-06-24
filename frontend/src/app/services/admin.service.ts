05eea6148493c3d395d2aeeda6e71526d1f5b3e6
Davidhzk2 <dherrera035@misena.edu.co>
2021-06-24 14:06:19 -0500

    admin service

---
+++ frontend/src/app/services/admin.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private env: String;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.env = environment.APP_URL;
+  }
+
+  registerUser(user: any) {
+    return this.http.post(this.env + "user/registerAdmin", user)
+  }
+
+  listUsers(){
+    return this.http.get(this.env + "user/listUser");
+  }
+
+  listRole() {
+    return this.http.get(this.env + "role/listRole");
+  }
+}
