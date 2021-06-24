43987865b599e8c0bd4f46f95315b0cf917f1238
Davidhzk2 <dherrera035@misena.edu.co>
2021-06-24 12:52:01 -0500

    Register user from admin with autorization

--- frontend/src/app/admin/register-user/register-user.component.ts
+++ frontend/src/app/admin/register-user/register-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from "../../services/admin.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register-user',
@@ -6,10 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user.component.css']
 })
 export class RegisterUserComponent implements OnInit {
+  registerData: any;
+  roleData: any;
+  successMessage: String;
+  errorMessage: String;
 
-  constructor() { }
+  constructor(private router: Router, private admin: AdminService) {
+    this.registerData = {};
+    this.roleData = {};
+    this.successMessage = '';
+    this.errorMessage = '';
+  }
 
   ngOnInit(): void {
+    this.admin.listRole().subscribe(
+      (res: any) => {
+        console.log(res);
+        this.roleData = res.role;
+        console.log(this.roleData);
+      },
+      (err) => {
+        console.log(err.error);
+
+        this.errorMessage = err.error;
+      }
+    )
+  }
+
+  registerUser() {
+    if (!this.registerData.name || !this.registerData.email || !this.registerData.password || !this.registerData.roleId) {
+      this.errorMessage = 'Incomplete Data.';
+      this.closeAlert();
+    } else {
+      this.admin.registerUser(this.registerData).subscribe(
+        (res: any) => {
+          console.log(res);
+          this.registerData = {};
+          this.router.navigate(['/listUser']);
+        },
+        (err) => {
+          this.errorMessage = err.error;
+          this.registerData = {};
+          this.closeAlert();
+        }
+      )
+    }
+  }
+
+  closeAlert() {
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 3000);
+  }
+  closeX() {
+    this.errorMessage = '';
   }
 
 }
