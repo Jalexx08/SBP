7da028117e3e3bc3665c6d5fab265409a3ed0534
Villadiego Hoyos <d.villadiego.hoyos@accenture.com>
2021-06-25 08:40:49 -0500

    S

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   private env: String;
-  public admin: boolean;
+  private admin: boolean;
 
   constructor(private http: HttpClient, private router: Router) {
     this.env = environment.APP_URL;
